587fd4992d189674e73315254080d8cbdac0db3a
taeyoung-no <notaeyoung03@gmail.com>
2026-06-04 13:38:57 +0900

    Option::setName, Option::setPrice

--- app/src/main/java/cafeorder/domain/Option.java
+++ app/src/main/java/cafeorder/domain/Option.java
@@ -26,4 +26,12 @@ public class Option {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
